da07d12b62dbd4fdb72ede045ef52a5f95329e3e
tahlilgar <s.hossein468@gmail.com>
2023-02-12 11:09:18 +0330

    added website template
    deleted old html files and added new templates and statics

--- website/views.py
+++ website/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'website\index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'website\about.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'website\contact.html')
+
+def elemenets(request):
+    return render(request, 'website\elements.html')
